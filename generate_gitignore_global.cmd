751698fb9e50047c6287c37c78f8d494592bc3d8
s-kocher <skocher@avb202.com>
2019-05-09 03:20:45 +0200

    Extract in variables the gitignore api parameters

--- generate_gitignore_global.cmd
+++ generate_gitignore_global.cmd
@@ -3,11 +3,16 @@
 REM Use https://www.gitignore.io/ API
 REM You need to have gi.cmd in path as described in https://docs.gitignore.io/install/command-line (End of Windows section)
 
-REM SET OUTPUT_DIR=.
-REM SET OUTPUT_DIR=%USERPROFILE%
 SET OUTPUT_DIR=%HOME%
+REM SET OUTPUT_DIR=%USERPROFILE%
+REM SET OUTPUT_DIR=.
+
 SET OUTPUT_NAME=.gitignore_global
 SET OUTPUT=%OUTPUT_DIR%\%OUTPUT_NAME%
+
+SET GI_OS=linux,windows,macos
+SET GI_PROG_LANGUAGES=java,node,maven
+SET GI_IDE=code,eclipse,intellij
 
 If not exist %OUTPUT% GOTO :generatefile
 
@@ -36,19 +41,19 @@ echo. >> %OUTPUT%
 echo #-----------------------------# >> %OUTPUT%
 echo #     ~Operating Systems~    -# >> %OUTPUT%
 echo #-----------------------------# >> %OUTPUT%
-CALL gi linux,windows,macos >> %OUTPUT%
+CALL gi %GI_OS% >> %OUTPUT%
 echo. >> %OUTPUT%
 
 echo #-----------------------------# >> %OUTPUT%
 echo #- ~Programmation languages~ -# >> %OUTPUT% 
 echo #-----------------------------# >> %OUTPUT%
-CALL gi java,node,maven >> %OUTPUT%
+CALL gi %GI_PROG_LANGUAGES% >> %OUTPUT%
 echo. >> %OUTPUT%
 
 echo #-----------------------------# >> %OUTPUT%
 echo #-   ~Code editors and IDE~  -# >> %OUTPUT%
 echo #-----------------------------# >> %OUTPUT%
-CALL gi code,eclipse,intellij >> %OUTPUT%
+CALL gi %GI_IDE% >> %OUTPUT%
 echo. >> %OUTPUT%
 
 @echo ON
